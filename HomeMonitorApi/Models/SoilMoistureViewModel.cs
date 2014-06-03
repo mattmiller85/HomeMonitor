f10faab894bc248484d46923bc1c940aa3b0aa94
millermt <mattmiller85@gmail.com>
2014-06-03 13:47:37 -0400

    Add soild moisture model

---
+++ HomeMonitorApi/Models/SoilMoistureViewModel.cs
@@ -0,0 +1,37 @@
+﻿using HomeMonitorDataAccess;
+using System;
+
+namespace HomeMonitorApi.Models
+{
+    public class SoilMoistureViewModel : IHomeMonitorApiModel<SoilMoistureReading>
+    {
+        public int SensorNumber { get; set; }
+        public DateTime Taken { get; set; }
+        public decimal MilliVolts { get; set; }
+
+        public static SoilMoistureViewModel FromData(SoilMoistureReading reading)
+        {
+            return new SoilMoistureViewModel
+            {
+                Taken = reading.Taken,
+                MilliVolts = reading.MilliVolts,
+                SensorNumber = reading.SensorNumber
+            };
+        }
+
+        public SoilMoistureReading ToData()
+        {
+            return new SoilMoistureReading
+            {
+                Taken = this.Taken,
+                MilliVolts = this.MilliVolts,
+                SensorNumber = this.SensorNumber
+            };
+        }
+
+        public bool IsValid
+        {
+            get { return SensorNumber > 0; }
+        }
+    }
+}
